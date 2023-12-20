345b35e866677d5d85c9f2a7ab976fdfc8a0614d
Meerims <mrmsdkv@gmail.com>
2023-12-20 22:53:35 +0000

    class1 files

---
+++ Class1/bucket.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket" "hello" {
+  bucket_prefix = "kaizen-"
+}
+
+resource "aws_s3_bucket" "hello2" {
+  bucket_prefix = "kaizen-hello123"
+}
+
+output "bucket" {
+  value = aws_s3_bucket.hello2.arn 
+  
+}
+
+
+output "bucket2" {
+  value = aws_s3_bucket.hello.arn 
+  
+}
