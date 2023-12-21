f02e09ae575b5f4ff4bf8aef476e166edad553c2
Meerims <mrmsdkv@gmail.com>
2023-12-21 07:11:12 +0000

    fixed key output

--- HW1/key.tf
+++ HW1/key.tf
@@ -1,12 +1,22 @@
+# resource "aws_key_pair" "my-key1" {
+#   key_name   = "my-key1"
+#   public_key = file("~/.ssh/id_rsa.pub")
+#  }
+
+#  output "my-key1" {
+#   value = aws_key_pair.my-key1
+  
+# }
+
 resource "aws_key_pair" "my-key1" {
   key_name   = "my-key1"
   public_key = file("~/.ssh/id_rsa.pub")
- }
-
- output "my-key1" {
-  value = aws_key_pair.my-key1
-  
 }
+
+output "key_type" {
+  value = aws_key_pair.my-key1.key_name
+}
+
 
 
 
